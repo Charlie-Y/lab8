1bb7b5714a5f8f5ba5370956023b2baf12814e15
vagrant <vagrant@precise32.(none)>
2014-02-27 19:45:47 +0000

    setup

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,9 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+
+
+    $('.likeBtn').on('click', function(){
+        ga("send", "event", "Like button", "Clicked");
+    })
 }
